bde7b7dd9ba4e848f331ae21883421494c818083
mata4pemp <mata4pemp@gmail.com>
2025-11-04 23:28:26 +0800

    receiptform edit receipt

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import NavBar from "/components/NavBar.jsx";
+import ReceiptList from "./components/AllReceiptsPage/ReceiptList";
 
 function App() {
   return (
     <Router>
       <NavBar />
       <Routes>
-        <Route path='/receipts' />
+        <Route path="/receipts" element={<ReceiptList />} />
       </Routes>
     </Router>
   );
