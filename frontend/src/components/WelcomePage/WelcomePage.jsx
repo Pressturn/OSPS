d8e399fef77e0a5026a9b7407d8ab5379f218e1b
Pressturn <Prestonyeo1@gmail.com>
2025-11-17 22:20:46 +0800

    Style: Added CSS to WelcomePage

--- frontend/src/components/WelcomePage/WelcomePage.jsx
+++ frontend/src/components/WelcomePage/WelcomePage.jsx
@@ -1,17 +1,21 @@
 import { Link } from 'react-router-dom'
+import "./WelcomePage.css"
 
 function WelcomePage() {
     return (
-        <div>
-            <h1>Welcome to OSPS</h1>
-            <p>Chase your friends for money with ease</p>
-            <div>
-                <Link to="/signup">
-                    <button>Sign Up</button>
-                </Link>
-                <Link to="/signin">
-                    <button>Sign In</button>
-                </Link>
+        <div className="welcome-container">
+            <div className="welcome-box">
+                <h1 className="welcome-title">Welcome to OSPS</h1>
+                <p className="welcome-subtitle">Online Split Payment System</p>
+                <p className="welcome-subtitle">Chase your friends for money with ease</p>
+                <div className="welcome-buttons">
+                    <Link to="/signup" className="welcome-btn btn-signup">
+                        Sign Up
+                    </Link>
+                    <Link to="/signin" className="welcome-btn btn-signin">
+                        Sign In
+                    </Link>
+                </div >
             </div >
         </div >
     )
